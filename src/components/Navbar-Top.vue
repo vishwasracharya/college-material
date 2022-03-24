e3024fc1f611ceb13b409caa383eee66f78ad9e2
Vishwas Acharya <vishwasracharya@gmail.com>
2022-03-24 21:01:17 +0530

    Basic Structure Done

---
+++ src/components/Navbar-Top.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <nav class="d-flex align-items-center justify-content-center">
+      <router-link to="/" class="tab m-1 p-1">Home</router-link>
+      <router-link to="/about" class="tab m-1 p-1">About</router-link>
+    </nav>
+  </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="my-5">
+      <h1>Vishwas BE IT</h1>
+      <p>
+        I made this website, just so that i can have a backup of all the
+        materials, notes of mine and share easily. Hence this website has all
+        the documents related to my BE-IT journey like Books, Notes,
+        Assignments, Test Papers, Other Materials.
+      </p>
+      <p>
+        It is just for sharing purpose, Not to make a inappropriate use of it,
+        hence it is just for the help of the students
+      </p>
+      <p> <strong>GitHub Repo:</strong> <a class="text-primary" href="https://github.com/vishwasracharya/Vishwas-BE-IT" target="_blank" rel="noopener noreferrer">https://github.com/vishwasracharya/Vishwas-BE-IT</a></p>
+      <li class="badge bg-light text-dark ms-0 mx-1">Syllabus</li>
+      <li class="badge bg-light text-dark mx-1">Assignments</li>
+      <li class="badge bg-light text-dark mx-1">Hand-Written Notes</li>
+      <li class="badge bg-light text-dark mx-1">Papers</li>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
